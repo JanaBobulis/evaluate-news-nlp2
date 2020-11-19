b695503893bc694bdc5ebb43fbec98c9434e508f
JanaBobulis <71527795+JanaBobulis@users.noreply.github.com>
2020-11-19 15:24:03 +0000

    Update urlChecker.spec.js

--- __test__/urlChecker.spec.js
+++ __test__/urlChecker.spec.js
@@ -1,14 +1,15 @@
 import { urlChecker } from '../src/client/js/urlChecker';
 
-describe('testing the function validateURL()', () => {
+describe('testing the function urlChecker()', () => {
     var url = "https://www.valentinog.com/blog/jest/";
        test('Returns true on valid url', async () => {
            const response = urlChecker(url);
            expect(response).toBe(true);
            expect(response).toBeDefined();
        })
-       //it('Returns false on invalid url', () => {
-       //    expect(response).toBe(false);
-       //});
+       test('Returns false on invalid url', async () => {
+            const response2 = urlChecker(falseInput);
+            expect(response2).toBe(false);
+       })
    })
 
